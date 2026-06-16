0571c028efaba09a4e457f081b6c02135aa9aebd
kishore070605 <kishoreshanmugam07@gmail.com>
2026-06-16 13:14:36 +0530

    changes

--- backend/notes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')        //importing express module
-const app = express()                     //creating an instance of express application
-app.get('/', (req,res) => {               //defining a route for the root URL
-    res.send('Hello World!')
-})
-app.listen(3000)                           //starting the server and listening on port 3000
